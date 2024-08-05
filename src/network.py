262bd5197d594d0421c2feee01d71284c78cfd63
rutger <rutgercappendijk@gmail.com>
2024-08-05 16:05:32 +0200

    added mlp and cleaned up stuff

--- src/network.py
+++ src/network.py
@@ -1,14 +1,81 @@
-from typing import NamedTuple, Sequence
+from typing import Sequence
 
-from numpy import ndarray
+from numpy import dot 
+from numpy.random import random
 
 
-class Perceptron(NamedTuple):
-    weights: ndarray[float]
+class Perceptron:
+    weights: Sequence[float]
     bias: float
 
-    def predict(self, inputs: Sequence[float]) -> bool:
-        value = sum(w * x for w, x in zip(self.weights, inputs)) + self.bias
-        if value > 0:
-            return True
-        return False
+    def __init__(self, weights: Sequence[float], bias: float):
+        assert isinstance(weights, list) and len(weights) > 0
+        assert all(isinstance(weight, float) or isinstance(weight, int) for weight in weights)
+        assert isinstance(bias, float) or isinstance(bias, int)
+        self.weights =  weights
+        self.bias = bias
+        self
+
+    def predict(self, inputs: Sequence[float]) -> float:
+        value = dot(self.weights, inputs) + self.bias
+        return 1 if value > 0 else 0
+
+
+class HiddenLayer:
+    _neurons: list[Perceptron]
+    size: int
+
+    def __init__(self, size: int):
+        assert isinstance(size, int) and size > 0
+        self.size = size
+        self._neurons = []
+
+    
+    def forward(self, inputs: Sequence[float]) -> Sequence[float]:
+        if len(self._neurons) == 0:
+            raise ValueError("Neurons uninitialized")
+        return [neuron.predict(inputs) for neuron in self._neurons]
+    
+    def initialize(self, input_size: int):
+        for _ in range(self.size):
+            weights = [random() for _ in range(input_size)]
+            bias = 0
+            self._neurons.append(Perceptron(weights, bias))
+
+
+class SoftmaxLayer:
+    size: int
+
+    def __init__(self, size: int):
+        assert isinstance(size, int) and size > 0
+        self.size = size
+
+    def forward(self, inputs: Sequence[float]) -> Sequence[float]:
+        return [0.0] * self.size
+
+
+class MultiLayerPerceptron:
+    input_size: int 
+    output_layer: SoftmaxLayer 
+    hidden_layers: Sequence[HiddenLayer]
+
+    def __init__(self, input_size: int, hidden_layers: Sequence[HiddenLayer], output_layer: SoftmaxLayer):
+        assert isinstance(input_size, int) and  input_size > 0 
+        assert isinstance(hidden_layers, list) and len(hidden_layers) > 0
+        self.input_size = input_size
+        self.hidden_layers = hidden_layers
+        self.output_layer = output_layer 
+    
+    def initialize(self):
+        input_size = self.input_size
+        for layer in self.hidden_layers:
+            layer.initialize(input_size)
+            input_size = layer.size
+            
+
+    def predict(self, inputs: Sequence[float]) -> Sequence[float]:
+        outputs = inputs
+        for layer in self.hidden_layers:
+            outputs = layer.forward(outputs)
+        return self.output_layer.forward(outputs)
+
--- test/test_network.py
+++ test/test_network.py
@@ -1,11 +1,17 @@
-from numpy import ndarray
+from pytest import fixture, raises
 
-from src.network import Perceptron
+from src.network import MultiLayerPerceptron, Perceptron, HiddenLayer, SoftmaxLayer
+
+@fixture
+def perceptron() -> Perceptron:
+    weights = [1, 1]
+    bias = 0
+    return Perceptron(weights, bias)
 
 
 def test_perceptron_returns_false_if_values_are_0() -> None:
     inputs = [0, 0]
-    weights = ndarray([1, 1])
+    weights = [1, 1]
     bias = 0
     perceptron = Perceptron(weights, bias)
 
@@ -14,9 +20,19 @@ def test_perceptron_returns_false_if_values_are_0() -> None:
     assert result == False
 
 
+def test_perceptron_raises_error_if_input_does_not_match_weights() -> None:
+    inputs = [0]
+    weights = [1, 1]
+    bias = 0
+    perceptron = Perceptron(weights, bias)
+
+    with raises(ValueError): 
+        perceptron.predict(inputs)
+
+
 def test_perceptron_returns_true_if_values_are_1() -> None:
     inputs = [1, 1]
-    weights = ndarray([1, 1])
+    weights = [1, 1]
     bias = 0
     perceptron = Perceptron(weights, bias)
 
@@ -27,7 +43,7 @@ def test_perceptron_returns_true_if_values_are_1() -> None:
 
 def test_perceptron_ignores_inputs_with_0_weight() -> None:
     inputs = [1, 0]
-    weights = ndarray([0, 1])
+    weights = [0, 1]
     bias = 0
     perceptron = Perceptron(weights, bias)
 
@@ -38,10 +54,31 @@ def test_perceptron_ignores_inputs_with_0_weight() -> None:
 
 def test_perceptron_returns_true_if_high_bias() -> None:
     inputs = [0, 0]
-    weights = ndarray([0, 0])
+    weights = [0, 0]
     bias = 1
     perceptron = Perceptron(weights, bias)
 
     result = perceptron.predict(inputs)
 
     assert result == True
+
+def test_perceptron_returns_false_if_low_bias() -> None:
+    inputs = [0, 0]
+    weights = [0, 0]
+    bias = -1
+    perceptron = Perceptron(weights, bias)
+
+    result = perceptron.predict(inputs)
+
+    assert result == False
+
+
+def test_can_create_network() -> None:
+    layer = HiddenLayer(size=1)
+    output_layer = SoftmaxLayer(size=2)
+    network = MultiLayerPerceptron(input_size=1, hidden_layers=[layer], output_layer=output_layer)
+    network.initialize()
+
+    result = network.predict([1])
+
+    assert len(result) == 2
