f60fa2d604656385fa83e40f399137a4422819e4
rutger <rutgercappendijk@gmail.com>
2024-09-04 10:24:12 +0200

    python version is now 3.10 and updated training program

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 from dataset import prep_data
 from network import MLP
+import random
+
+random.seed(42)
 
 if __name__ == "__main__":
     print("Splitting data set...")
@@ -9,7 +12,7 @@ if __name__ == "__main__":
     X_val, y_val = prep_data(Path("test.csv"))
     model = MLP(input_size=X.shape[1], hidden_layer_sizes=[24, 24, 24], output_size=2)
     overview = model.train(
-        (X, y), learning_rate=0.0001, epochs=10000, validation_data=(X_val, y_val), batch_size=128
+        (X, y), learning_rate=0.0001, epochs=50000, validation_data=(X_val, y_val), batch_size=256
     )
     print("Training complete in {:.2f} seconds".format(overview.duration))
     overview.plot_loss()
